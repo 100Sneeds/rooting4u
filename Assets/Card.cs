d83e94d0015ac12c86059d4c5f2e460d2cc0bcc1
Jalapinecone <jalapinecone@gmail.com>
2023-02-03 01:00:12 -0500

    added some card shit

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public int statDefence;
+    public int statAttack;
+    public int colourDefence;
+    public int colourAttack;
+
+    public Card(){
+        statDefence = Random.Range(0,5);   // Init with random damage value
+        statAttack = Random.Range(0,5);    // Init with random damage value
+        colourDefence = Random.Range(0,3); // Init with random colour value
+        colourAttack = Random.Range(0,3);  // Init with random colour value
+    }
+
+    // Play
+    public PlayCard(){
+
+    }
+
+        int CalculateDamageToDefender(Card attacker, Card defender){
+        if (attacker.colourAttack == defender.colourDefence){
+            return Mathf.Clamp(attacker.statAttack - defender.statDefence, 0, 10);
+        }
+        return attacker.statAttack;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
