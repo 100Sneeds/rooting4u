f1bdf5d0ae29e7f78e1e8a2ef940a8b0c2b967bf
Colin Tong <tongcolin@live.ca>
2023-02-03 19:23:03 -0500

    Add missing return type

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -9,34 +9,33 @@ public class Card : MonoBehaviour
     public int colourDefence;
     public int colourAttack;
 
-    public Card(){
-        statDefence = Random.Range(0,5);   // Init with random damage value
-        statAttack = Random.Range(0,5);    // Init with random damage value
-        colourDefence = Random.Range(0,3); // Init with random colour value
-        colourAttack = Random.Range(0,3);  // Init with random colour value
-    }
-
-    // Play
-    public PlayCard(){
-
-    }
-
-        int CalculateDamageToDefender(Card attacker, Card defender){
-        if (attacker.colourAttack == defender.colourDefence){
-            return Mathf.Clamp(attacker.statAttack - defender.statDefence, 0, 10);
-        }
-        return attacker.statAttack;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        statDefence = Random.Range(0, 5);   // Init with random damage value
+        statAttack = Random.Range(0, 5);    // Init with random damage value
+        colourDefence = Random.Range(0, 3); // Init with random colour value
+        colourAttack = Random.Range(0, 3);  // Init with random colour value
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Play
+    public void PlayCard()
+    {
+
+    }
+
+    int CalculateDamageToDefender(Card attacker, Card defender)
+    {
+        if (attacker.colourAttack == defender.colourDefence)
+        {
+            return Mathf.Clamp(attacker.statAttack - defender.statDefence, 0, 10);
+        }
+        return attacker.statAttack;
     }
 }
