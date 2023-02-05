8da1b54c129bb4e66d7795d59f578ef383903c4c
Colin Tong <tongcolin@live.ca>
2023-02-05 01:16:31 -0500

    Add lerp to score indicator

--- Assets/Scenes/MatchScene/ProgressIndicator.cs
+++ Assets/Scenes/MatchScene/ProgressIndicator.cs
@@ -7,25 +7,49 @@ public class ProgressIndicator : MonoBehaviour
     public ProgressBar progressBar;
 
     private static float X_OFFSET_PER_POINT = 0.97f;
+    private static float POSITION_LERP_DURATION_SECONDS = 0.5f;
 
     private Vector3 originalPosition;
+
+    private Vector3 sourceVector;
+    private Vector3 destinationVector;
+    private float lerpTimer = 0;
+    private int previousScore;
 
     // Start is called before the first frame update
     void Start()
     {
+        previousScore = this.progressBar.GetScore();
         originalPosition = this.transform.position;
+        sourceVector = originalPosition;
+        destinationVector = originalPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = this.GetPosition();
+        lerpTimer += Time.deltaTime;
+
+        int currentScore = this.progressBar.GetScore();
+        bool isScoreChanged = currentScore != previousScore;
+        previousScore = currentScore;
+
+        if (isScoreChanged)
+        {
+            sourceVector = this.transform.position;
+            destinationVector = this.GetDestinationPositionForScore(currentScore);
+            lerpTimer = 0;
+        }
+
+        float lerpPercent = lerpTimer / POSITION_LERP_DURATION_SECONDS;
+        this.transform.position = Vector3.Lerp(sourceVector, destinationVector, lerpPercent);
+        Debug.Log(this.transform.position);
     }
 
-    private Vector3 GetPosition()
+    private Vector3 GetDestinationPositionForScore(int score)
     {
         float originalX = originalPosition.x;
-        float newX = originalX + progressBar.GetScore() * X_OFFSET_PER_POINT;
+        float newX = originalX + score * X_OFFSET_PER_POINT;
         return new Vector3(newX, originalPosition.y, originalPosition.z);
     }
 }
