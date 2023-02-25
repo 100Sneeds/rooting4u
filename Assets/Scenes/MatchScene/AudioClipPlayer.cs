c6411fc78ede0826ff857bd1b07040f31808ddc8
ColinT <tongcolin@live.ca>
2023-02-25 11:58:25 -0500

    Sync backing track (#79)

--- Assets/Scenes/MatchScene/AudioClipPlayer.cs
+++ Assets/Scenes/MatchScene/AudioClipPlayer.cs
@@ -19,15 +19,24 @@ public class AudioClipPlayer : MonoBehaviour
         //"Medium3",
         //"Hard1",
         //"Hard2",
+        "Backbeat"
     };
+
+    [Serializable]
+    public struct NamedAudioClip
+    {
+        public string name;
+        public AudioClip audioClip;
+    }
+    public NamedAudioClip[] namedAudioClips;
 
     private Dictionary<string, AudioClip> audioClips;
     private AudioSource audioSource;
 
     // Start is called before the first frame update
-    async void Start()
+    void Start()
     {
-        this.audioClips = await this.LoadAudioClips(AUDIO_CLIP_FILE_NAMES);
+        this.audioClips = this.GetAudioClipsFromNamedArray(namedAudioClips);
         this.audioSource = this.GetComponent<AudioSource>();
     }
 
@@ -43,75 +52,24 @@ public class AudioClipPlayer : MonoBehaviour
         this.audioSource.PlayOneShot(audioClip);
     }
 
+    public void PlayAudioClipByName(string audioClipName, float volume)
+    {
+        var audioClip = this.GetAudioClip(audioClipName);
+        this.audioSource.PlayOneShot(audioClip, volume);
+    }
+
+    private Dictionary<string, AudioClip> GetAudioClipsFromNamedArray(NamedAudioClip[] namedAudioClips)
+    {
+        var audioClips = new Dictionary<string, AudioClip>();
+        foreach (var namedAudioClip in namedAudioClips)
+        {
+            audioClips.Add(namedAudioClip.name, namedAudioClip.audioClip);
+        }
+        return audioClips;
+    }
+
     private AudioClip GetAudioClip(string audioClipName)
     {
         return this.audioClips.GetValueOrDefault(audioClipName);
-    }
-
-    private async Task<Dictionary<string, AudioClip>> LoadAudioClips(string[] fileNames)
-    {
-        string audioClipDirectory = Path.Combine(Application.dataPath, "Sounds");
-        string[] paths = AUDIO_CLIP_FILE_NAMES.Select((fileName) => Path.Combine(audioClipDirectory, fileName + ".ogg")).ToArray();
-
-        var audioClips = new Task<AudioClip>[paths.Length];
-        for (int pathIndex = 0; pathIndex < paths.Length; pathIndex++) {
-            var path = paths[pathIndex];
-            var audioClip = LoadAudioClip(path, AudioType.OGGVORBIS);
-            audioClips[pathIndex] = audioClip;
-        }
-
-        var audioClipDictionary = new Dictionary<string, AudioClip>();
-        for (int fileNameIndex = 0; fileNameIndex < paths.Length; fileNameIndex++)
-        {
-            var fileName = fileNames[fileNameIndex];
-            var audioClip = await audioClips[fileNameIndex];
-            audioClipDictionary.Add(fileName, audioClip);
-        }
-
-        return audioClipDictionary;
-    }
-
-    private async Task<AudioClip> LoadAudioClip(string path, AudioType audioType)
-    {
-        AudioClip audioClip = null;
-        int retryDelayMs = 5;
-        var builder = new UriBuilder(path) { Scheme = Uri.UriSchemeFile };
-        var uri = builder.ToString();
-        using (UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip(uri, audioType))
-        {
-            webRequest.SendWebRequest();
-
-            // wrap tasks in try/catch, otherwise it'll fail silently
-            try
-            {
-                while (!webRequest.isDone)
-                {
-                    await Task.Delay(retryDelayMs);
-                    retryDelayMs *= 2; // exponential backoff
-                    if (retryDelayMs >= 8000)
-                    {
-                        throw new Exception("Timeout error: Failed to load audio clip after 8 seconds");
-                    }
-                }
-
-                bool isConnectionError = webRequest.result == UnityWebRequest.Result.ConnectionError;
-                bool isProtocolError = webRequest.result == UnityWebRequest.Result.ProtocolError;
-
-                if (isConnectionError || isProtocolError)
-                {
-                    Debug.Log($"{webRequest.error}");
-                }
-                else
-                {
-                    audioClip = DownloadHandlerAudioClip.GetContent(webRequest);
-                }
-            }
-            catch (Exception err)
-            {
-                Debug.Log($"{err.Message}, {err.StackTrace}");
-            }
-        }
-
-        return audioClip;
     }
 }
